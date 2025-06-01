774119594c0481b1aa7809d3cf6a45357460c08a
Jack Barrett <jjbdev21@gmail.com>
2025-06-01 21:03:06 +0100

    Upgrade tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import flowbiteReact from "flowbite-react/plugin/vite";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import flowbiteReact from "flowbite-react/plugin/vite"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), flowbiteReact()],
+  plugins: [react(), flowbiteReact(), tailwindcss()],
 })
