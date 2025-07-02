9cd6ec65b728633194cdeb9985cd60d82d1bc696
Jack Barrett <jjbdev21@gmail.com>
2025-07-02 10:03:41 +0100

    Banks endpoint return short name

--- backend/api/banks.go
+++ backend/api/banks.go
@@ -5,14 +5,14 @@ import "github.com/gofiber/fiber/v2"
 func (s *Server) GetBanks(c *fiber.Ctx) error {
 	banks := []Bank{}
 
-	rows, err := s.DB.Query(c.Context(), `SELECT id, name, fixed_products, csv_import_enabled, api_import_enabled FROM "bank"`)
+	rows, err := s.DB.Query(c.Context(), `SELECT id, name, short_name, fixed_products, csv_import_enabled, api_import_enabled FROM "bank"`)
 	if err != nil {
 		return DBError(c, err)
 	}
 
 	for rows.Next() {
 		bank := Bank{}
-		if err := rows.Scan(&bank.Id, &bank.Name, &bank.FixedProducts, &bank.CsvImportEnabled, &bank.ApiImportEnabled); err != nil {
+		if err := rows.Scan(&bank.Id, &bank.Name, &bank.ShortName, &bank.FixedProducts, &bank.CsvImportEnabled, &bank.ApiImportEnabled); err != nil {
 			return DBError(c, err)
 		}
 		banks = append(banks, bank)
