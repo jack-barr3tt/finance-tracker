72f3239788f2b132e1a38c3d2d2f2cf9f56412ce
Jack Barrett <jjbdev21@gmail.com>
2026-03-01 18:48:09 +0000

    Fix themes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { initThemeMode } from "flowbite-react"
+import App from "./App.tsx"
+import "./index.css"
 
-createRoot(document.getElementById('root')!).render(
+initThemeMode()
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
