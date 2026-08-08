5b4488a46b4d91e7def4273c0cfd537e2d325ccf
Jack Barrett <jjbdev21@gmail.com>
2026-08-08 20:55:16 +0100

    Auto focus email field

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -38,6 +38,7 @@ export default function Login() {
       >
         <h1 className="w-full text-2xl font-medium text-center">Login</h1>
         <TextInput
+          autoFocus
           type="email"
           placeholder="Email"
           value={email}
